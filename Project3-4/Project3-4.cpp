a96769155a320e1080dff34606bd1c0613f8e8c2
Chase McCluskey <c-mccluskey.1@onu.edu>
2024-09-12 09:21:30 -0400

    Add project files.

---
+++ Project3-4/Project3-4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int carcost;
+	double mpg;
+	int miles;
+	double gasprice;
+	int yearsused;
+	int resalevalue;
+	int totalcost;
+
+	cout << "Enter the cost of car:";
+	cin >> carcost;
+	cout << "Enter MPG: ";
+	cin >> mpg;
+	cout << "Enter miles driven per year: ";
+	cin >> miles;
+	cout << "Enter gas price: ";
+	cin >> gasprice;
+	cout << "Enter years of use: ";
+	cin >> yearsused;
+	cout << "Enter the resale amount: ";
+	cin >> resalevalue;
+	cout << endl;
+	totalcost = carcost + ((miles) / mpg * gasprice) * yearsused - resalevalue;
+	cout << "Net cost of car ownership: " << totalcost;
+}
